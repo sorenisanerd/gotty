24549dca015d507c6977786199bcc0b9c3a7873f
llaoj <qustwwy@163.com>
2022-12-11 18:00:15 +0800

    1. Adjusted the log format 2. optimized code

--- webtty/webtty.go
+++ webtty/webtty.go
@@ -23,6 +23,7 @@ type WebTTY struct {
 	windowTitle []byte
 	arguments   map[string][]string
 	permitWrite bool
+	writeLog    bool
 	columns     int
 	rows        int
 	reconnect   int // in seconds
@@ -91,14 +92,18 @@ func (wt *WebTTY) Run(ctx context.Context) error {
 	go func() {
 		errs <- func() error {
 			buffer := make([]byte, wt.bufferSize)
-			line := ""
+			var line string
+			arguments, err := json.Marshal(wt.arguments)
+			if err != nil {
+				return err
+			}
 			for {
 				n, err := wt.masterConn.Read(buffer)
 				if err != nil {
 					return ErrMasterClosed
 				}
 
-				err = wt.handleMasterReadEvent(buffer[:n], &line)
+				err = wt.handleMasterReadEvent(buffer[:n], &line, arguments)
 				if err != nil {
 					return err
 				}
@@ -167,7 +172,7 @@ func (wt *WebTTY) masterWrite(data []byte) error {
 	return nil
 }
 
-func (wt *WebTTY) handleMasterReadEvent(data []byte, line *string) error {
+func (wt *WebTTY) handleMasterReadEvent(data []byte, line *string, argument []byte) error {
 	if len(data) == 0 {
 		return errors.New("unexpected zero length read from master")
 	}
@@ -188,15 +193,14 @@ func (wt *WebTTY) handleMasterReadEvent(data []byte, line *string) error {
 			return errors.Wrapf(err, "failed to decode received data")
 		}
 
-		// log.Printf("[wlog] %v %X\n", decodedBuffer[:n], decodedBuffer[:n])
-		utils.FormatWritesLog(decodedBuffer[:n], line)
-		if decodedBuffer[n-1] == 13 {
-			argumentsByte, err := json.Marshal(wt.arguments)
-			if err != nil {
-				return errors.Wrapf(err, "failed to marshal arguments map")
+		if wt.writeLog {
+			utils.FormatWriteLog(decodedBuffer[:n], line)
+			// 13(ASCII) means carriage return(CR)
+			// it is the end of a line
+			if decodedBuffer[n-1] == 13 {
+				log.Printf("[write-log] %s %s\n", argument, *line)
+				*line = ""
 			}
-			log.Printf("[wlog] %s %s\n", *line, string(argumentsByte))
-			*line = ""
 		}
 
 		_, err = wt.slave.Write(decodedBuffer[:n])
